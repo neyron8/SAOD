719dad42c62b29d5537b871bab8ba80cd0706349
neyron8 <32508305+neyron8@users.noreply.github.com>
2018-04-11 21:15:32 +0700

    Update bstree.h

--- bstree.h
+++ bstree.h
@@ -1,11 +1,12 @@
 #ifndef BSTREE_C
 #define BSTREE_C
 
-typedef struct {
+typedef struct
+{
 	char *key;
-	int value;
 	struct bstree *left;
 	struct bstree *right;
+	struct bstree *top;
 
 }bstree;
 
@@ -13,12 +14,14 @@ typedef struct
 {
   char *field; 
   struct list *ptr; 
-}list;
 
-bstree *bstree_create (char *key, int value);
+} list;
+
+bstree *bstree_create (bstree *root,char *key);
 void bstree_add (bstree *tree, char *key);
 bstree *bstree_lookup (bstree *tree, char *key);
 bstree *bstree_min (bstree *tree);
 bstree *bstree_max (bstree *tree); 
+bstree *bstree_create_root(char *key4f);
 
 #endif
