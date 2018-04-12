0ef13f5d1d930689f1903aefce3454e837f4aff5
neyron8 <32508305+neyron8@users.noreply.github.com>
2018-04-12 16:38:40 +0700

    Update main.c

--- main.c
+++ main.c
@@ -7,20 +7,20 @@ int main()
 {
 	bstree * test;
 	bstree * find;
-    test = bstree_create_root("jacko");
-	bstree_add(test,"fdfdfdfd");
-	bstree_add(test,"sh");
-	bstree_add(test,"cvxz");
-	bstree_add(test,"cv");
-	bstree_add(test,"arrt");
-	bstree_add(test,"kjh");
-	bstree_add(test,"jackodfgdfg");
-	bstree_add(test,"jackodfgdfgccvbv");
-	bstree_add(test,"dfdfhcvcv");
-	find = bstree_lookup(test, "arrt");
-	printf("HERE WE GO %s\n", find -> key);
-	
+    test = bstree_create("jacko", 43);
+	bstree_add(test,"fdfdfdfd", 13);
+	bstree_add(test,"sh", 154);
+	bstree_add(test,"cvxz", 123);
+	bstree_add(test,"cv", 5);
+	bstree_add(test,"arrt", 76);
+	bstree_add(test,"kjh", 1893);
+	bstree_add(test,"jackodfgdfg", 12333);
+	bstree_add(test,"jackodfgdfgccvbv", 1093);
+	bstree_add(test,"dfdfhcvcv", 12313);
+	bstree_add(test,"999999999999", 12313);
 	treeprint(test);
+	find = bstree_max (test);
+	printf("MINIMUM  %s", find -> key);	
 	
     return 0;
 }
