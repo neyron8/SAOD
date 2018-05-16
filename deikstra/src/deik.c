1ead7c9fa91e9963f6d3e5b0a63b13662987b4a8
Nick Durakov <dark.lords@mail.ru>
2018-05-16 22:54:03 +0700

    BEZUSHODNOST

--- deikstra/src/deik.c
+++ deikstra/src/deik.c
@@ -1,7 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+
+	int size;
+	int apex;
+
+} heapnode;
+
+typedef struct {
+
+	int nnodes;
+	int maxsize;
+	heapnode *nodes;
+	int metka;
+
+} heap;
+
+heap *heap_create(int maxsize)
+{
+	heap *h;
+
+	h = malloc(sizeof(heap));
+	if (h != NULL) {
+		h->maxsize = maxsize;
+		h->nnodes = 0;
+		h->nodes = malloc(sizeof(heapnode) * (maxsize + 1));
+		if (h->nodes == NULL) {
+			free(h);
+			return NULL;
+		}
+	}
+
+	return h;
+}
 
 void heap_swap(heapnode *a, heapnode *b)
 {
-
 	heapnode temp;
 	temp = *a;
 	*a = *b;
@@ -24,115 +60,136 @@ int heap_insert(heap *h, int size, int apex)
 	return 0;
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+void heap_heapify(heap *h, int index)
+{
+	for (;;) {
+		int left = 2 * index;
+		int right = 2 * index + 1;
+		int smallest = index;
+		if (left <= h->nnodes && h->nodes[left].size < h->nodes[index].size ) {
+			smallest = left;
+		}
+		if (right <= h->nnodes && h->nodes[right].size < h->nodes[smallest].size) {
+			smallest = right;
+		}
+		if (smallest == index) {
+			break;
+		}
+		heap_swap(&h->nodes[index], &h->nodes[smallest]);
+		index = smallest;
+	}
+}
 
-typedef struct {
+heapnode heap_extract_min(heap *h)
+{
+	if (h->nnodes == 0) {
+		return;
+	}
+	heapnode minnode = h->nodes[1];
+	h->nodes[1] = h->nodes[h->nnodes];
+	h->nnodes--;
+	heap_heapify(h, 1);
+	return minnode;
+}
 
-  int size;
-  int apex;
+int get_num (heap *h, int index)
+{
+	int i = 1;
+	for (i; i < 21; i++){
+		if (h->nodes[i].apex == index){
+			break;
+		}	
+	}
+	return i;
+}
 
-} D;
+int heap_decrease_key(heap *h, int index, int size)
+{
+	if (h->nodes[index].size < size) {
+		return -1;
+	}
+	h->nodes[index].size = size;
 
-typedef struct {
+	for (; index > 1 && h->nodes[index].size < h->nodes[index / 2].size; index = index / 2) {
+		heap_swap(&h->nodes[index], &h->nodes[index / 2]);
+  	}
 
-  int nnodes;
-  int maxsize;
-  struct D **nodes;
-  int metka;
+	return index;
+}
 
-} heap;
+void deikstra()
+{
+    FILE *in;
+	int j,k,temp,i,minindex,min,n,count;
+    in = fopen ( "matrix.txt", "rt");
+	int matrix[21][21];
+	for (i = 1; i < 21; i++) {
+		for (j = 1; j < 21; j++) {
+			fscanf(in, "%d", &matrix[i][j]);
+		}
+	}
+	int metka[21]; 
+	int path[21];
+	heap *structure;
+	structure = heap_create(20);
+	for (k = 1; k < 21; k++){
+		heap_insert(structure, 90000, k);
+		metka[k] = 1;
+	}
+	for (k = 1; k < 21; k++){
+		if (structure -> nodes[k].apex == 1){
+			structure -> nodes[k].size = 0;
+			break;   
+		}
+	}  
 
-heap *heap_create(int maxsize) {
-  heap *h;
-  h = malloc(sizeof(heap));
-  if (h != NULL) {
-    h->maxsize = maxsize;
-    h->nnodes = 0;
-    h->nodes = malloc(sizeof(int) * (maxsize + 1));
-    if (h->nodes == NULL) {
-      free(h);
-      return NULL;
+    while (1) {
+     
+		heap_heapify(structure, 1);
+        minindex = 90000;
+        min = 90000;
+		if ((metka[structure -> nodes[1].apex] == 1) ) {
+			printf("%d\n",structure -> nodes[1].size );
+			min = structure -> nodes[1].size;
+			minindex = structure -> nodes[1].apex;
+			path[structure -> nodes[1].apex] = structure -> nodes[1].size;
+			heap_extract_min(structure);
+		}
+        
+        if (minindex != 90000) {
+            for (i = 1; i < 21; i++) {
+                if (matrix[minindex][i] > 0 && metka[i] != 0) {
+                    temp = min + matrix[minindex][i];
+					n = get_num(structure, i);
+                    if (temp < structure -> nodes[n].size){
+                    	heap_decrease_key(structure,n, temp);
+					}
+                }
+            }
+            metka[minindex] = 0;
+        }
+
+		count = 0;
+		for (n = 1; n < 21; n++){
+			if (metka[n] == 0){
+				count++;
+			}
+		}
+
+		if (count == 20) {
+			//for (k = 1; k < 21; k++){
+				//printf("%d\n", path[i]);
+			//}
+			break;
+		}
+
     }
-  }
-  return h;
+
 }
 
-void heap_swap(D *a, D *b) {
-  D temp;
-  temp = *a;
-  *a = *b;
-  *b = temp;
-}
-
-int heap_insert(heap *h, int size, int apex) {
-  if (h->nnodes >= h->maxsize) {
-    return -1;
-  }
-  h->nnodes++;
-  h->nodes[h->nnodes].size = size;
-  h->nodes[h->nnodes].apex = apex;
-
-  for (int i = h->nnodes; i > 1 && h->nodes[i].size < h->nodes[i / 2].size;
-       i = i / 2) {
-
-    heap_swap(&h->nodes[i], &h->nodes[i / 2]);
-  }
-  return 0;
-}
-
-D heap_extract_min(heap *h) {
-  if (h->nnodes == 0) {
-    return NULL;
-  }
-  D minnode = h->nodes[1];
-  h->nodes[1] = h->nodes[h->nnodes];
-  h->nnodes--;
-  heap_heapify(h, 1);
-  return minnode;
-}
-
-int heap_decrease_key(heap *h, int index, int size) {
-  if (h->nodes[index].size < size)
-    return -1;
-  h->nodes[index].size = size;
-  for (; index > 1 && h->nodes[index].size > h->nodes[index / 2].size;
-       index = index / 2) {
-    heap_swap(&h->nodes[index], &h->nodes[index / 2]);
-  }
-  return index;
-}
-
-void heap_heapify(heap *h, int index) {
-  for (;;) {
-    int left = 2 * index;
-    int right = 2 * index + 1;
-    // Find largest key: A[index], A[left] and A[right]
-    int smallest = index;
-    if (left <= h->nnodes && h->nodes[left].size < h->nodes[index].size) {
-      smallest = left;
-    }
-    if (right <= h->nnodes && h->nodes[right].size < h->nodes[smallest].size) {
-      smallest = right;
-    }
-    if (smallest == index) {
-      break;
-    }
-
-    heap_swap(&h->nodes[index], &h->nodes[largest]);
-    index = largest;
-  }
-}
-
-int main() {
-  heap *test;
-  test = heap_create(5);
-  test->nnodes = 1;
-  printf("%d", test->nnodes);
-  test->nodes = malloc(sizeof(int) * 5);
-  test->nodes[test->nnodes] = 123;
-  printf("%d\n", test->nodes[test->nnodes]);
-
-  return 0;
+int main()
+{		
+	deikstra();
+	
+	return 0;
 }
